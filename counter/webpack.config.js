9d9d14bdbd60e6b7ec3fb35428142555712d1c9d
cryptolemming <aliayoub22@gmail.com>
2015-07-20 18:46:33 -0400

    finished installing libraries

--- counter/webpack.config.js
+++ counter/webpack.config.js
@@ -1,9 +1,62 @@
 var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
 
-module.exports = {
-	entry: path.resolve(__dirname, 'app/main'),
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'bundle.js'
-	}
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+  entry: [path.resolve(ROOT_PATH, 'app/main')],
+  resolve: {
+  	extensions: ['', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      },
+    ]
+  },
+  plugins: [
+  	new HtmlwebpackPlugin({
+  		title: 'Counter'
+  	})
+  ]
 };
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel?stage=1',
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    },
+  });
+}
+
+if(TARGET === 'dev') {
+  module.exports = merge(common, {
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:8080',
+      'webpack/hot/dev-server'
+    ],
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['react-hot', 'babel?stage=1'],
+          include: path.resolve(ROOT_PATH, 'app'),
+        },
+      ],
+    },
+  });
+}
